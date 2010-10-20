04f8bdf6253dbf8b075fa81145511d85045e3a7d
NIIBE Yutaka <gniibe@fsij.org>
2010-10-20 10:14:03 +0900

    Added "CQ STARM" target.

---
+++ boards/CQ_STARM/board.c
@@ -0,0 +1,36 @@
+#include "config.h"
+#include "ch.h"
+#include "hal.h"
+
+#include "../common/hwinit.c"
+
+void
+hwinit0 (void)
+{
+  hwinit0_common ();
+}
+
+void
+hwinit1 (void)
+{
+  hwinit1_common ();
+}
+
+void
+USB_Cable_Config (FunctionalState NewState)
+{
+  /* CQ STARM has no functionality to stop USB.  */
+  /*
+   * It seems that users can add the functionality with USB_DC (PD9)
+   * though
+   */
+}
+
+void
+set_led (int value)
+{
+  if (value)
+    palClearPad (IOPORT3, GPIOC_LED);
+  else
+    palSetPad (IOPORT3, GPIOC_LED);
+}
---
+++ boards/CQ_STARM/board.mk
@@ -0,0 +1,6 @@
+# List of all the board related files.
+BOARDSRC = ../boards/CQ_STARM/board.c \
+	../boards/common/hw_config.c
+
+# Required include directories
+BOARDINC = ../boards/CQ_STARM
