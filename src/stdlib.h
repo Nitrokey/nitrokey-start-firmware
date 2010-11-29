35f421fc349b4750fe60167db2873b180148c057
NIIBE Yutaka <gniibe@fsij.org>
2010-11-30 08:40:01 +0900

    try to remove newlib dependency

--- src/stdlib.h
+++ src/stdlib.h
@@ -4,12 +4,7 @@
 
 typedef unsigned int size_t;
 
-#ifdef REPLACE_MALLOC
-#define malloc my_malloc
-#define free my_free
-#define realloc my_realloc
-#endif
-
-extern void *malloc (size_t size);
-extern void free (void *ptr);
-extern void *realloc (void *ptr, size_t size);
+#include "ch.h"
+#include "chheap.h"
+#define malloc(size)	chHeapAlloc (NULL, size)
+#define free(p)		chHeapFree (p)
