313464f907db9db8f740f65cba8bfa2c241c90ca
NIIBE Yutaka <gniibe@fsij.org>
2013-07-19 12:16:14 +0900

    fix cleaning up memory before free

--- src/gnuk.h
+++ src/gnuk.h
@@ -236,12 +236,12 @@ extern struct key_data kd[3];
 #endif
 
 extern int rsa_sign (const uint8_t *, uint8_t *, int, struct key_data *);
-extern const uint8_t *modulus_calc (const uint8_t *, int);
-extern void modulus_free (const uint8_t *);
+extern uint8_t *modulus_calc (const uint8_t *, int);
+extern void modulus_free (uint8_t *);
 extern int rsa_decrypt (const uint8_t *, uint8_t *, int, struct key_data *);
 extern int rsa_verify (const uint8_t *pubkey, const uint8_t *hash,
 		       const uint8_t *signature);
-extern const uint8_t *rsa_genkey (void);
+extern uint8_t *rsa_genkey (void);
 
 extern const uint8_t *gpg_do_read_simple (uint8_t);
 extern void gpg_do_write_simple (uint8_t, const uint8_t *, int);
