3926f42647acf8a0fa270d534f1631af4a29f898
NIIBE Yutaka <gniibe@fsij.org>
2015-06-03 16:27:41 +0900

    add test/ecc_nistp256_keys.py

---
+++ test/ecc_nistp256_keys.py
@@ -0,0 +1,31 @@
+# Data taken from: 
+#    A. Jivsov, Sample Keys and Messages:
+#    https://sites.google.com/site/brainhub/pgpecckeys
+
+# uid       ec_dsa_dh_256 <openpgp@brainhub.org>
+# sign key:
+# nistp256/BAA59D9C 2010-09-17
+# keygrip: 8E06A180EFFE4C65B812150CAF19BF30C0689A4C
+#
+# q=(x, y) and d
+key[0] = (0x0bc7a7baebd5f08c74c77b71ee44e7bb0b5a18317b996da5393e33acc52932c6,
+          0xd2f60f4d1efe35a0b9fb8d3787ed4bee97ca012d07b8f5835be7093545d532e6,
+          0xd8f28c530c99821faa5ee2ff4dd8d1df01995d4e98fb45f8768cb65abd4adaa9)
+
+# decryption key:
+# sub   nistp256/4089AB73 2010-09-17 nistp256
+# keygrip: E4403F3FD7A443FAC29FEF288FA0D20AC212851E
+#
+# q=(x, y) and d
+key[1] = (0x7f70c0a8184cdcaea5db20ba8fed17e47bdefb744d575ec449130af37edade65,
+          0x8ae7ee35d20e8897911c9f564be33d9a94bc1e5c927b1aa07ff750d2d11c2971,
+          0xa05cd14749bea3f3d14c92dc438e45e351efe860360c431705b7d42410581843)
+
+# auth key from: uid       ec_dsa_dh_256_no_pass <openpgp@brainhub.org>
+#
+# q=(x, y) and d
+key[2] = (0x81fbbc20eea9e8d1c3ceabb0a8185925b113d1ac42cd5c78403bd83da19235c6,
+          0x5ed6db13d91db34507d0129bf88981878d29adbf8fcd1720afdb767bb3fcaaff,
+          0xa355916f8665eb99c1af48d9560b5c6889e5287bc75aa693aaae9bdb15e8b3fd)
+
+# This file is here to extend the test suite for ECC.
