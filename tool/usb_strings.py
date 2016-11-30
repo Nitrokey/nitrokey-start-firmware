7e9ba71cb23d85123f144efabc2623a19bded448
Szczepan Zalega <szczepan@nitrokey.com>
2016-11-30 17:44:42 +0100

    Remove doubled code for getting GNUK device
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- tool/usb_strings.py
+++ tool/usb_strings.py
@@ -24,19 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import usb, sys
 
-USB_VENDOR_FSIJ=0x234b
-USB_PRODUCT_GNUK=0x0000
-
-def gnuk_devices_by_vidpid():
-    busses = usb.busses()
-    for bus in busses:
-        devices = bus.devices
-        for dev in devices:
-            if dev.idVendor != USB_VENDOR_FSIJ:
-                continue
-            if dev.idProduct != USB_PRODUCT_GNUK:
-                continue
-            yield dev
+from gnuk_token import gnuk_devices_by_vidpid
 
 field = ['', 'Vendor', 'Product', 'Serial', 'Revision', 'Config', 'Sys', 'Board']
 
