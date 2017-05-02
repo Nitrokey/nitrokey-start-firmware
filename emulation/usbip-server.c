6dcb4dd0279ba5f4683e1bb44214a2d3156e7192
NIIBE Yutaka <gniibe@fsij.org>
2017-05-02 15:05:15 +0900

    Add usb-emu.c.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- emulation/usbip-server.c
+++ emulation/usbip-server.c
@@ -1,20 +1,20 @@
-/*                                                    -*- coding: utf-8 -*-
+/*
  * usbip-server.c - USB Device Emulation by USBIP Protocol
  *
- * Copyright (C) 2017  Free Software Initiative of Japan
+ * Copyright (C) 2017  Flying Stone Technology
  * Author: NIIBE Yutaka <gniibe@fsij.org>
  *
- * This file is a part of Gnuk, a GnuPG USB Token implementation.
+ * This file is a part of Chopstx, a thread library for embedded.
  *
- * Gnuk is free software: you can redistribute it and/or modify it
+ * Chopstx is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Gnuk is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
- * License for more details.
+ * Chopstx is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
