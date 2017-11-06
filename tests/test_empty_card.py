c9b1e511ba5018cf0ebaa875462e178d7bca9d2b
NIIBE Yutaka <gniibe@fsij.org>
2017-11-06 10:51:11 +0900

    Fix test and tests.

--- tests/test_empty_card.py
+++ tests/test_empty_card.py
@@ -146,7 +146,7 @@ def test_historical_bytes(card):
 
 def test_extended_capabilities(card):
     a = get_data_object(card, 0xc0)
-    assert a == None or match(b'[\x70\x74]\x00\x00\x20[\x00\x08]\x00\x00\xff\x01\x00', a)
+    assert a == None or match(b'[\x70\x74\x75]\x00\x00\x20[\x00\x08]\x00\x00\xff\x01\x00', a)
 
 def test_algorithm_attributes_1(card):
     a = get_data_object(card, 0xc1)
