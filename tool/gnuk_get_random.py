126283b1ac7e3dc184a54b3aa587e30a2fce058a
NIIBE Yutaka <gniibe@fsij.org>
2019-02-24 15:12:12 +0900

    Add count for tool/gnuk_get_random.py.

--- tool/gnuk_get_random.py
+++ tool/gnuk_get_random.py
@@ -5,11 +5,17 @@ from binascii import hexlify
 import sys
 
 if __name__ == '__main__':
+    count = 0
     gnuk = get_gnuk_device()
     gnuk.cmd_select_openpgp()
     looping = (len(sys.argv) > 1)
     while True:
-        challenge = gnuk.cmd_get_challenge().tostring()
+        try:
+            challenge = gnuk.cmd_get_challenge().tostring()
+        except Exception as e:
+            print(count)
+            raise e
         print(hexlify(challenge))
+        count = count + 1
         if not looping:
             break
