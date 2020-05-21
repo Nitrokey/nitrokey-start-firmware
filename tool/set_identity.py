09790d0d6fe06d0f7f41ff4e5080f871c469d035
Szczepan Zalega <szczepan@nitrokey.com>
2020-05-21 11:34:19 +0200

    SI: reformat and fix typos

--- tool/set_identity.py
+++ tool/set_identity.py
@@ -7,18 +7,18 @@ from subprocess import check_output
 from gnuk_token import get_gnuk_device
 from usb.core import USBError
 
-if __name__=="__main__":
-    if len(sys.argv)!=2:
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
         print("missing identity number")
         sys.exit(1)
     if not sys.argv[1].isdigit():
         print("identity number must be a digit")
         sys.exit(1)
-    identity=int(sys.argv[1])
-    if(identity<0 or identity>2):
+    identity = int(sys.argv[1])
+    if identity < 0 or identity > 2:
         print("identity must be 0, 1 or 2")
         sys.exit(1)
-    print("Trying to set identity to %d"%(identity,))
+    print("Trying to set identity to %d" % (identity,))
     for x in range(3):
         try:
             gnuk = get_gnuk_device()
@@ -27,14 +27,13 @@ if __name__=="__main__":
                 gnuk.cmd_set_identity(identity)
             except USBError:
                 print("device has reset, and should now have the new identity")
-                sys.exit(0);
-                
+                sys.exit(0)
+
         except ValueError as e:
             if 'No ICC present' in str(e):
                 print("Could not connect to device, trying to close scdaemon")
                 result = check_output(["gpg-connect-agent",
-                                           "SCD KILLSCD", "SCD BYE", "/bye"])
+                                       "SCD KILLSCD", "SCD BYE", "/bye"])
                 time.sleep(3)
             else:
                 print('*** Found error: {}'.format(str(e)))
-    
