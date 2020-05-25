309a994ee093c05f5189d9490af3533fd5141673
Kliment Yanev <kliment.yanev@gmail.com>
2020-05-25 14:12:43 +0200

    Add usage message to set_identity.py

--- tool/set_identity.py
+++ tool/set_identity.py
@@ -12,16 +12,24 @@ import sys, binascii, time, os
 import rsa
 from struct import pack
 
+def usage():
+    print("Usage: \npython3 set_identity.py id")
+    print("id must be 0, 1, or 2")
+
 if __name__=="__main__":
+    print("set_identity.py selects identity on a nitrokey start")
     if len(sys.argv)!=2:
         print("missing identity number")
+        usage();
         sys.exit(1)
     if not sys.argv[1].isdigit():
         print("identity number must be a digit")
+        usage();
         sys.exit(1)
     identity=int(sys.argv[1])
     if(identity<0 or identity>2):
         print("identity must be 0, 1 or 2")
+        usage();
         sys.exit(1)
     print("Trying to set identity to %d"%(identity,))
     for x in range(3):
