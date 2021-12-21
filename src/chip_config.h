236b5154f49aa8aa8e6a06d9e289cf28fe3579f3
lennard.boediger <lennard@nitrokey.com>
2021-12-21 16:02:04 +0100

    hw_rv flag 2

--- src/chip_config.h
+++ src/chip_config.h
@@ -1,13 +1,16 @@
 #include "stddef.h"
+#include <stdint.h>
 
 #ifndef NITROKEY_START_FIRMWARE_CHIP_CONFIG
 #define NITROKEY_START_FIRMWARE_CHIP_CONFIG
 
+extern uint8_t hw_rev;
+
 struct HardwareDefinition{
     struct {
-        int i_DELIBARATELY_DO_IT_WRONG_START_STOP;
+        uint8_t i_DELIBARATELY_DO_IT_WRONG_START_STOP;
         int i_STM32_USBPRE;
-        int i_STM32_PLLMUL_VALUE;
+        uint8_t i_STM32_PLLMUL_VALUE;
         int i_STM32_ADCPRE;
     } clock;
 };
